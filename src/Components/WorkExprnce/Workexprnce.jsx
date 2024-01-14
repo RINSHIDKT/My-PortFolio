91c59417ed5cf320434cd44810fb2876ada993a4
shamil <shamilmohd418@gmail.com>
2024-01-14 11:33:58 +0530

    dxfxdf

--- src/Components/WorkExprnce/Workexprnce.jsx
+++ src/Components/WorkExprnce/Workexprnce.jsx
@@ -11,11 +11,16 @@ const Workexprnce = () => {
         <div className="expirience_left">
           <div className="contents">
           <h4>Synnefo Solutions Pvt.ltd</h4>
-            <p>2023</p>
+            <p>June 2023 - January 2024</p>
           </div>
         </div>
         <div className="line"></div>
-        <div className="expirience_right"></div>
+        <div className="expirience_right">
+            <div className="contents">
+            <h4>MERN Stack Developer</h4>
+            <p>I have successfully completed a 6-month internship with the MERN stack in Synnefo Solutions Pvt.Ltd.</p>
+            </div>
+        </div>
     </div>
     </div>
   )
