9783bd008e50e94c5560a994db56045a5e4b84aa
shamil <shamilmohd418@gmail.com>
2024-01-14 11:15:12 +0530

    zdczd

--- src/Components/WorkExprnce/Workexprnce.jsx
+++ src/Components/WorkExprnce/Workexprnce.jsx
@@ -4,7 +4,19 @@ import './WorkExpence.scss'
 const Workexprnce = () => {
   return (
     <div className='wrkExperienceMain'>
-     
+    <div className="heading">
+    <h1>My Work <span>Expirience</span></h1>
+    </div>
+    <div className="expirience">
+        <div className="expirience_left">
+          <div className="contents">
+          <h4>Synnefo Solutions Pvt.ltd</h4>
+            <p>2023</p>
+          </div>
+        </div>
+        <div className="line"></div>
+        <div className="expirience_right"></div>
+    </div>
     </div>
   )
 }
