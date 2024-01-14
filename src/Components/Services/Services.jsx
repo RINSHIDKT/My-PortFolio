d088201632d875dd2b97cd4b957e1eba117b0aa2
shamil <shamilmohd418@gmail.com>
2024-01-14 10:21:37 +0530

    kugyjky

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -7,7 +7,7 @@ const Services = () => {
     <div className='servises-main-div'>
        {/* //////////////SERVICES/////////////// */}
   <div className="services-main">
-        <h1 className='service-heading'>My Awesome <span className='srvs-text'>Services.</span></h1>
+        {/* <h1 className='service-heading'>My Awesome <span className='srvs-text'>Services.</span></h1> */}
        <div className="service_main_carrdss">
         <div className="service-main-crd-left">
         <div className="service-card-main">
@@ -16,7 +16,17 @@ const Services = () => {
             <span><HiOutlineDesktopComputer className='pers_icon' /></span>
             </div>
             <div className="service-card-right">
-             <h3> Website Designing</h3>
+             <h3> UI/UX Designing</h3>
+             <p>5+ Projects </p>
+            </div>
+            </div>
+
+            <div className="service-card">
+            <div className="service-card-left">
+            <span><HiOutlineDesktopComputer className='pers_icon' /></span>
+            </div>
+            <div className="service-card-right">
+             <h3>Web Development</h3>
              <p>4+ Projects </p>
             </div>
             </div>
@@ -24,7 +34,12 @@ const Services = () => {
         </div>
 
         <div className="service-main-crd-right">
-
+            <h1>What do I <span>help?</span></h1>
+            <p>I will help you with finging a solution and solve your problem, We use process design to create digital products. Besides that also help their business.</p>
+            <div className="project_completed">
+              <h1>9+</h1>
+              <p>Project Completed</p>
+            </div>
         </div>
        </div>
       </div>
